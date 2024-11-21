ce682a500f01c54935f79ac3ed79f1695704ca87
basiljilani <basiljilani@gmail.com>
2024-11-21 23:42:21 +0500

    final hopefully

--- src/config/amplify.ts
+++ src/config/amplify.ts
@@ -3,9 +3,9 @@ import { Amplify } from 'aws-amplify';
 Amplify.configure({
   Auth: {
     Cognito: {
-      region: import.meta.env.Region,
-      userPoolId: import.meta.env.User-ID,
-      userPoolClientId: import.meta.env.Client-ID
+      region: import.meta.env.REGION,
+      userPoolId: import.meta.env.USER_ID,
+      userPoolClientId: import.meta.env.CLIENT_ID
     }
   }
 });
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,12 +1,12 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-    readonly Region: string
-    readonly User-ID: string
-    readonly Client-ID: string
-    readonly API-Gateway: string
+    readonly REGION: string;
+    readonly USER_ID: string;
+    readonly CLIENT_ID: string;
+    readonly API_GATEWAY: string;
   }
   
   interface ImportMeta {
-    readonly env: ImportMetaEnv
+    readonly env: ImportMetaEnv;
   }
